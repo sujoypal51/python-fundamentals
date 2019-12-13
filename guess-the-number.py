a4e218b19fad2843361b5443273bc12a4f2eb0d9
John Timmers <johnctimmersjr@gmail.com>
2019-12-13 10:59:07 -0500

    import random number, request guess, and print response

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,1 +1,11 @@
-print('hello world')
+import random
+number = random.randint(0, 21)
+
+guess = input('Guess what number I am thinking of: ')
+
+if int(guess) == number:
+    print('Your guess is correct.')
+elif int(guess) > number:
+    print('Your guess is greater than the number I am thinking of.')
+else:
+    print('Your guess is less than the number I am thinking of.')
