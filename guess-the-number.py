ea2e112c02b105ff341495b02d3da58a335b152a
John Timmers <johnctimmersjr@gmail.com>
2020-01-19 08:46:42 -0500

    generate random number between zero and 20, allow user to guess the random number up to five times

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,11 +1,16 @@
 import random
 number = random.randint(0, 21)
-
-guess = input('Guess what number I am thinking of: ')
-
+number_of_guesses = 0
+guess = input('I am thinking of a number between 1 and 20. Guess what number I am thinking of: ')
 if int(guess) == number:
     print('Your guess is correct.')
-elif int(guess) > number:
-    print('Your guess is greater than the number I am thinking of.')
-else:
-    print('Your guess is less than the number I am thinking of.')
+while (not guess == number) and (number_of_guesses < 6):
+    number_of_guesses += 1
+    if int(guess) > number:
+        print('Your guess is greater than the number I am thinking of.')
+        guess = input('Guess again! ')
+    elif int(guess) < number:
+        print('Your guess is less than the number I am thinking of.')
+        guess = input('Guess again! ')
+    else:
+        print('Your guess is correct!')
